4bbf7081bbf6f9232c4fe80d591e81f04443f35b
Developer-X <161570896+Karandeep-Tamrakar@users.noreply.github.com>
2025-03-05 09:46:25 +0530

    Components and files
    Includes steup files

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+export default {
+  dialect: "postgresql",
+  schema: "./utils/schema.jsx",
+  out: "./drizzle",
+  dbCredentials: {
+    url: "postgresql://neondb_owner:npg_bJ18LAFhIpso@ep-shrill-mouse-a1aqgxim-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require",
+    connectionString:
+      "postgresql://neondb_owner:npg_bJ18LAFhIpso@ep-shrill-mouse-a1aqgxim-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require",
+  },
+};
